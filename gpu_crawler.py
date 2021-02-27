a500f12ef8728edd35a20bd37e855b3f81c5b964
Adel Bibi <bibiadel93@gmail.com>
2021-02-27 10:43:59 +0000

    added card exception option

--- gpu_crawler.py
+++ gpu_crawler.py
@@ -3,6 +3,7 @@ import GPUtil as GPU
 
 parser = argparse.ArgumentParser(description='GPU Scheduler')
 parser.add_argument("--memory_per_gpu", type=int, default=11, help="specify free per gpu memory requirement in GB")
+parser.add_argument("--card_exception", nargs='+', type=str, default=[''], help="specify names to exclude")
 
 args = parser.parse_args()
 
@@ -13,13 +14,10 @@ def main():
     GPUs = GPU.getGPUs()
     for i in range(len(GPUs)):
         gpu = GPUs[i]
-        if (gpu.load*100.0 < 20.0) and (gpu.memoryUtil*100 <= 10.0) and (gpu.memoryFree/GB >= args.memory_per_gpu):
-            # print("GPU RAM Free: {0:.0f}MB | Used: {1:.0f}MB | Util {2:3.0f}% | Total {3:.0f}MB".format(gpu.memoryFree, gpu.memoryUsed, gpu.memoryUtil*100, gpu.memoryTotal))
-            gpu_IDs.append(i)
+        if gpu.name not in args.card_exception:
+            if (gpu.load*100.0 < 20.0) and (gpu.memoryUtil*100 <= 10.0) and (gpu.memoryFree/GB >= args.memory_per_gpu):
+                gpu_IDs.append(i)
     print(gpu_IDs)
-    # filehandle = open('GPUAvail.txt', 'w')
-    # for gpu_num in gpu_IDs:
-    #     filehandle.write(str(gpu_num) + '\n')
 
 
 if __name__ == "__main__":
