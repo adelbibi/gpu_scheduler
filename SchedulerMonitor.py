e0fa5be4609fdeaf2626befa996621a7ce91afae
Adel Bibi <bibiadel93@gmail.com>
2021-02-27 12:42:58 +0000

    minro changes

--- SchedulerMonitor.py
+++ SchedulerMonitor.py
@@ -1,6 +1,6 @@
 
 from fabric import Connection
-
+import time
 # torrnode3 > issues with password
 # torrnode6 > gpus down
 
@@ -60,25 +60,38 @@ class Scheduler(object):
         c.run(my_command, hide=True)
 
 
-
 def main():
     miniSLURM = Scheduler()
     miniSLURM.ping_avail_gpus()
-    
-    
-    
-    dict_all = {}
+    print('Hostname with largest free GPUs without exception cards: {}'.format(miniSLURM.hostname))
+    print('Corresponding GPU Indices on this Node that are free: {}'.format(miniSLURM.gpu_indices))
+
+    # if len(args.task_id) == 1 and args.task_id[0] == -1:
+    #     while(completed_jobs < len(task_list)):
+    #         my_all_str = ''
+    #         for i in range(min(miniSLURM.num_gpus, len(task_list) - completed_jobs)):
+    #             if i == 0:
+    #                 my_all_str += get_python_command_run(miniSLURM.gpu_indices[i], completed_jobs)
+    #             else:
+    #                 my_all_str += '&' + get_python_command_run(miniSLURM.gpu_indices[i], completed_jobs)
+    #             completed_jobs += 1
+    #         final = f'cd geometric_certification ; conda activate geo_rs ;  ' + my_all_str
+    #         print('#######################')
+    #         print('Running Jobs on Node: {}'.format(miniSLURM.hostname))
+    #         print(final)
+    #         miniSLURM.launch_job(final)
+    #         miniSLURM.ping_avail_gpus()
+    # def get_python_command_run(gpu_num, task_id):
+    #     my_str_command = ''
+    #     my_str_command += f'CUDA_VISIBLE_DEVICES={gpu_num} python train.py ' + get_task(task_id, False)  + ' --experiment-name ' + str(task_id)
+    #     return my_str_command
 
 
-    for host in hosts:
-        print(host)
-        c = Connection(host)
-        result = c.run('python gpu_crawler.py --memory_per_gpu 11', hide=True)
-        all_IDs = result.stdout.replace('[', '').replace(']','').replace('\n', ''). replace(' ', '')
-        all_IDs = all_IDs.split(',')
-        dict_all[host] = all_IDs
-    print(dict_all)
-
+    # def get_task(index, verbose):
+    #     task_params = dict(zip(all_params.keys(), task_list[index]))
+    #     if verbose:
+    #         print(task_params)
+    #     return ' '.join(f'--{k} {v}' for k, v in task_params.items())
 
 if __name__ == "__main__":
     main()
